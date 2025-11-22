cb6b482d0e5a9dd31b7e9d803050ab3d8c6c3fe2
Henrique Demézio <rickdemeziioofc@gmail.com>
2025-11-22 16:04:18 -0300

    Initial structure

---
+++ app/Commands/MakeModule.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Commands;
+
+use CodeIgniter\CLI\BaseCommand;
+
+class MakeModule extends BaseCommand
+{
+    public function run(array $params)
+    {
+        // Implementation of the command
+    }
+}
